742d1ddad7fc2e26e1bbe79de3bfad9a3c31d392
Giovanni Juliao <gjuliao32@gmail.com>
2023-05-02 15:32:35 -0400

    Added actions, store and reducer

--- app/javascript/react/src/store/index.js
+++ app/javascript/react/src/store/index.js
@@ -1,7 +1,7 @@
-import { createStore, applyMiddleware } from 'redux';
+import { configureStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/index';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = configureStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
